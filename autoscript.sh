e10a5ed27e87cde1c29dbcdd39dd7f228de412d7
ElDainosor <eldainosor@gmail.com>
2017-01-09 13:25:04 -0300

    Tesla update

--- autoscript.sh
+++ autoscript.sh
@@ -1,14 +1,14 @@
 echo "############################"
 echo "#########Auto-script tool #########"
-echo "###########For XOSP ###########"
+echo "###########For Tesla ###########"
 echo "############################"
 echo ""
 echo "Initializing cm repo and surnia stuff"
-repo init -u https://github.com/XOSP-Project/platform_manifest -b xosp-n
+repo init -u https://github.com/TeslaRom-N/manifest.git -b n7.1.1
 curl --create-dirs -L -o .repo/temp.sh -O -L https://raw.githubusercontent.com/eldainosor/dtest_web/84090aaed3506eb399e1b919a3fb0cfebf269acf/assets/oldlogo.png
 bash .repo/temp.sh
 rm .repo/temp.sh
-curl --create-dirs -L -o .repo/local_manifests/local_manifest.xml -O -L https://raw.githubusercontent.com/Aidonnou-surnia/shishu_.repo_local_manifests/xosp_n/local_manifest.xml
+curl --create-dirs -L -o .repo/local_manifests/local_manifest.xml -O -L https://raw.githubusercontent.com/Aidonnou-surnia/shishu_.repo_local_manifests/tesla_n/local_manifest.xml
 echo ""
 echo "Starting to sync. If you want to stop this you can use CTRL+C."
 sleep 10
